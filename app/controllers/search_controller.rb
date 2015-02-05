e730cd081ea38f4a4705336f3972fff20ac907d7
hyeunny <hyeunjoon.kim@mail.mcgill.ca>
2015-02-05 00:07:40 +0000

    better search implemented

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,13 @@
 class SearchController < ApplicationController
   def index
-    title = params[:title]
-    director = params[:director]
+    title_or_director = params[:title_or_director]
     runtime = params[:runtime_in_minutes].to_i
 
-    query = ""
+    query = "(title like '#{params[:title_or_director]}%' or director like '#{params[:title_or_director]}%')"
 
-    query += "title like '#{title}%'" if !title.blank?
-    query += "director like '#{director}%'" if title.blank? && !director.blank?
-    query += " and director like '#{director}%'" if !director.blank? && !title.blank?
-    query += " and runtime_in_minutes < 90" if runtime < 90 && (title || director)
-    query += " and runtime_in_minutes >= 90 and runtime_in_minutes <= 120" if runtime >= 90 && runtime <= 120 && (title || director)
-    query += " and runtime_in_minutes > 120" if runtime > 120 && (title || director)
+    query += " and runtime_in_minutes < 90" if runtime < 90
+    query += " and runtime_in_minutes >= 90 and runtime_in_minutes <= 120" if runtime >= 90 && runtime <= 120
+    query += " and runtime_in_minutes > 120" if runtime > 120
 
     @movies = Movie.where(query)
 
